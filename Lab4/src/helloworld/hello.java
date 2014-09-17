1ff13e73e71f7b797247bf27482bbb7b0ef02af8
jraesly <john.raesly@gmail.com>
2014-09-17 14:16:51 -0600

    Update hello.java

--- Lab4/src/helloworld/hello.java
+++ Lab4/src/helloworld/hello.java
@@ -3,6 +3,6 @@ package helloworld;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string.
-		System.out.println("How are you?");
+		System.out.println("How are you?"); // Display the string.
 	}
 }
